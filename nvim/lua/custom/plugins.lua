e86920b7832e83ffe8a3ca537fbf2d92e768f8e4
shridhar <shuheihisagi314@gmail.com>
2024-06-13 00:01:03 +0530

    adding nvim dot files

---
+++ nvim/lua/custom/plugins.lua
@@ -0,0 +1,59 @@
+-- custom plugins table
+
+local plugins = {
+  {
+    "lervag/vimtex",
+    lazy = false,
+    --  calls the plugin
+    init = function()
+    -- default pdf viewer app
+    vim.g['vimtex_view_method'] = "zathura"
+    -- compiler options to pass when compiling .tex files
+    vim.g.vimtex_compiler_latexmk = {
+    options = {
+        '-verbose',
+        '-file-line-error',
+        '-synctex=1',
+        '-interaction=nonstopmode',
+        -- this is for .svg files for the \includesvg function
+        '-shell-escape',
+      },
+    }
+    end
+  },
+  -- override base lspconfig
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      -- load custom configs
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end,
+  },
+  -- LSP
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "clangd",
+        "pyright",
+        "texlab"
+      }
+    }
+  },
+  -- over ride base nvim.cmp config
+  {
+    "hrsh7th/nvim-cmp",
+    opts = function()
+      return require "custom.configs.cmp"
+    end,
+    config = function(_, opts)
+      require("cmp").setup(opts)
+    end,
+  },
+  -- to customize cmp's autosuggestions, and add vs code style glyphs
+  {
+    "onsails/lspkind.nvim",
+  }
+}
+return plugins
